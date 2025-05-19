0d0e55d98b104bd687bad49d33bada4ab76b464a
Adarsh <adarsh.chavan@techstalwarts.com>
2025-05-19 14:51:34 +0530

    new 1

--- src/test/java/PageObjects/StatutoryApprovalManual_Class.java
+++ src/test/java/PageObjects/StatutoryApprovalManual_Class.java
@@ -23,8 +23,8 @@ public class StatutoryApprovalManual_Class {
 	@FindBy(xpath = "//li[text()=' Manual ']")
 	WebElement Manual_Statutory_Approval;
 
-//	@FindBy(xpath = "//span[text()='government form']")
-//	WebElement government_form;
+	@FindBy(xpath = "//span[text()='government form']")
+	WebElement government_form;
 
 	@FindBy(xpath = "//span[text()='Sec 33/38']")
 	WebElement Sec_33_38;
@@ -35,7 +35,7 @@ public class StatutoryApprovalManual_Class {
 	@FindBy(xpath = "//span[text()='Confirm']")
 	WebElement Confirm_button;
 
-	// -------------Government Annexure ----------------------------------
+	// -------------Government Annexure - Basic Info ----------------------------------
 
 	@FindBy(xpath = "(//span[@class='p-element p-dropdown-label p-inputtext ng-star-inserted'])[1]")
 	WebElement PocketName_Dropdown;
@@ -43,10 +43,70 @@ public class StatutoryApprovalManual_Class {
 	@FindBy(xpath = "(//span[@class='p-element p-dropdown-label p-inputtext ng-star-inserted'])[2]")
 	WebElement SocietyName_Dropdown;
 
-	@FindBy(xpath = "//span[text()='Select unit']")
+	@FindBy(xpath = "(//span[@class='p-element p-dropdown-label p-inputtext ng-star-inserted'])[3]")
 	WebElement unitNumber_Dropdown;
 
-//	@FindBy(xpath = "//button[text()='Next']")
-//	WebElement NextButton;
+	@FindBy(xpath = "//button[text()='Next']")
+	WebElement NextButton;
+	
+	// -------------Government Annexure - Approvals ----------------------------------
+	
+	@FindBy(xpath = "//input[@placeholder='Enter Sr. no.']")
+	WebElement BasicInfoAnnex_no;
+	
+	@FindBy(xpath = "//input[@placeholder='Enter Authority Name']")
+	WebElement Authority_Name;
+	
+	@FindBy(xpath = "//input[@placeholder='Enter Issued By Whom']")
+	WebElement Issued_By_Whom;
+	
+	@FindBy(xpath = "//input[@placeholder='Enter Reference No.']")
+	WebElement IssuedByReferenceNo;
+	
+	@FindBy(xpath = "//input[@placeholder='Select Reference Date']")
+	WebElement IssuedByReferenceDate;
+	
+	@FindBy(xpath = "//button[text()='Next']")
+	WebElement ApprovalsNextButton;
+	
+	@FindBy(xpath = "//input[@formcontrolname='annexCurrentoccupierFirstName']")
+	WebElement CurrentOccupier_FirstName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='annexCurrentoccupierMiddleName']")
+	WebElement CurrentOccupier_MiddleName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='annexCurrentoccupierLastName']")
+	WebElement CurrentOccupier_LastName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='cencusPresentoccupierFirstName']")
+	WebElement presentOccupier_FirstName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='cencusPresentoccupierMiddleName']")
+	WebElement presentOccupier_MiddleName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='cencusPresentoccupierLastName']")
+	WebElement presentOccupier_LastName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='cencusedFirstName']")
+	WebElement Census_FirstName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='cencusedMiddleName']")
+	WebElement Census_MiddleName;
+	
+	@FindBy(xpath = "//input[@formcontrolname='cencusedLastName']")
+	WebElement Census_LastName;
+	
+	@FindBy(xpath = "")
+	WebElement Annex_no;
+	
+	@FindBy(xpath = "")
+	WebElement Annex_no;
+	
+	@FindBy(xpath = "")
+	WebElement Annex_no;
+	
+	@FindBy(xpath = "")
+	WebElement Annex_no;
+	
 
 }
