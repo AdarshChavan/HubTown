3798f0f970f918a4933544c9f513fed0bc68d5db
Adarsh <adarsh.chavan@techstalwarts.com>
2025-06-11 18:47:37 +0530

    Society Master Happy Flow Completed

--- src/test/java/PageObjects/SocietyMaster_Class.java
+++ src/test/java/PageObjects/SocietyMaster_Class.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,81 +19,131 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.github.dockerjava.api.exception.NotFoundException;
+
 import Library.UtilityClass;
 
 public class SocietyMaster_Class {
-	
+
 	public WebDriver driver;
 	public WebDriverWait wait;
+	private static final Logger logger = LogManager.getLogger(PocketMaster_Class.class);
+
 	public SocietyMaster_Class(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
-	
-	@FindBy(xpath = "//div//ul//li//a//span[text()='Master']")
+
+	@FindBy(xpath = "//span[text()='Master']")
 	WebElement Master_Tab;
-	
+
 	@FindBy(xpath = "//span[text()='Society/Chawl']")
 	WebElement society_tab;
-	
+
 	@FindBy(xpath = "//span[text()=' New Society ']")
 	WebElement New_Society_button;
-	
+
 	@FindBy(xpath = "//span[text()='Select Pocket']")
 	WebElement Pocket_Name_Dropdown;
 
 	@FindBy(xpath = "//button[text()='Next']")
 	WebElement Next_button;
-	
+
 	@FindBy(xpath = "//div[@class='overflow-y-auto']")
 	WebElement Left_Menu;
-	
-	public void ClickOnTheMasterTab() {
-		
-		wait.until(ExpectedConditions.visibilityOf(Left_Menu));
-		WebElement scrollableContainer = driver.findElement(By.cssSelector(".overflow-y-auto"));
 
-		((JavascriptExecutor) driver).executeScript("arguments[0].scrollTop += 1000;", scrollableContainer);
-		
-		wait.until(ExpectedConditions.visibilityOf(Master_Tab));
-		
-		Master_Tab.click();
-		
-	}
+	@FindBy(xpath = "//input[@role='searchbox']")
+	WebElement DropdownSearchBox;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Society Name']")
+	WebElement SocietyName;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Village']")
+	WebElement Village;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Taluka']")
+	WebElement Taluka;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Owner Name']")
+	WebElement OwnerName;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Units']")
+	WebElement NoOfUnits;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Value']")
+	WebElement Area;
+
+	@FindBy(xpath = "//input[@placeholder='Enter Name']")
+	WebElement DataVerifiedBy;
+
+	@FindBy(xpath = "//textarea[@placeholder='Enter CTS No']")
+	WebElement CTSNo;
+
+	@FindBy(xpath = "//span[text()='Save']")
+	WebElement SaveButton;
 	
+	@FindBy(xpath = "//span[text()='Save Progress']")
+	WebElement SaveProgress;
+
+	public void ClickOnMasterTab() throws IOException, InterruptedException {
+		Thread.sleep(2000);
+		Master_Tab.click();
+
+	}
+
 	public void clickOntheScietyTab() {
 		society_tab.click();
 	}
-	
+
 	public void ClickOntheNewSocietyButton() throws InterruptedException {
 		Thread.sleep(2000);
 		New_Society_button.click();
 	}
-	
-	public void SelectPocketName() throws IOException{
+
+	public void SelectPocketName() throws IOException {
 		Pocket_Name_Dropdown.click();
-		String Pocket_Name = UtilityClass.read("pocketName");
-		List<WebElement> pocketnames = driver.findElements(By.xpath("//span[@class='ng-star-inserted']"));
-		
-		for(WebElement pocketname: pocketnames) {
-			if(pocketname.getText().equalsIgnoreCase("Pocket_Name"));
-			pocketname.click();
-			return;
-		}
+		DropdownSearchBox.sendKeys(UtilityClass.read("pocketName"));
+		DropdownSearchBox.sendKeys(Keys.ARROW_DOWN);
+		DropdownSearchBox.sendKeys(Keys.ENTER);
 	}
-	
-	
-	public void ClickOnNextButton() {
-		Next_button.click();
+
+	public void EnterSocietyName() throws IOException {
+		SocietyName.sendKeys(UtilityClass.read("SocietyName"));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	public void EnterVillageName() throws IOException {
+		Village.sendKeys(UtilityClass.read("Village"));
+	}
+
+	public void EnterTalukaName() throws IOException {
+		Taluka.sendKeys(UtilityClass.read("Taluka"));
+	}
+
+	public void EnterOwnerName() throws IOException {
+		OwnerName.sendKeys(UtilityClass.read("OwnerName"));
+	}
+
+	public void EnterNoOfUnits() throws IOException {
+		NoOfUnits.sendKeys(UtilityClass.read("NoOfUnits"));
+	}
+
+	public void EnterArea() throws IOException {
+		Area.sendKeys(UtilityClass.read("Area"));
+	}
+
+	public void EnterDataVerifiedBy() throws IOException {
+		DataVerifiedBy.sendKeys(UtilityClass.read("DataVerifiedBy"));
+	}
+
+	public void EnterCTSNo() throws IOException {
+		CTSNo.sendKeys(UtilityClass.read("CTSNo"));
+	}
+
+	public void ClickOntheSaveButton() {
+		SaveButton.click();
+		SaveProgress.click();
+
+	}
+
 }
--- src/test/java/TestClasses/SocietyMaster_TestClass.java
+++ src/test/java/TestClasses/SocietyMaster_TestClass.java
@@ -6,35 +6,71 @@ import org.testng.annotations.Test;
 
 import PageObjects.SocietyMaster_Class;
 
-public class SocietyMaster_TestClass extends BaseTest{
+public class SocietyMaster_TestClass extends BaseTest {
 
-	
-	
 	@Test(priority = 1)
-	public void clickOnSocietyTab() {
-		societyMaster_Class.ClickOnTheMasterTab();
+	public void clickOnSocietyTab() throws IOException, InterruptedException {
+		societyMaster_Class.ClickOnMasterTab();
 	}
-	
+
 	@Test(priority = 2)
 	public void ClickOntheScietyTab() {
 		societyMaster_Class.clickOntheScietyTab();
 	}
-	
+
 	@Test(priority = 3)
 	public void ClickOntheNewSocietyButton() throws InterruptedException {
 		societyMaster_Class.ClickOntheNewSocietyButton();
 	}
-	
+
 	@Test(priority = 4)
 	public void SelectPocketName() throws IOException {
 		societyMaster_Class.SelectPocketName();
 	}
-	
+
 	@Test(priority = 5)
-	public void ClickOnNextButton() {
-		societyMaster_Class.ClickOnNextButton();
+	public void enterSocietyName() throws IOException {
+		societyMaster_Class.EnterSocietyName();
 	}
-	
-	
+
+	@Test(priority = 6)
+	public void enterVillageName() throws IOException {
+		societyMaster_Class.EnterVillageName();
+	}
+
+	@Test(priority = 7)
+	public void enterTalukaName() throws IOException {
+		societyMaster_Class.EnterTalukaName();
+	}
+
+	@Test(priority = 8)
+	public void enterOwnerName() throws IOException {
+		societyMaster_Class.EnterOwnerName();
+	}
+
+	@Test(priority = 9)
+	public void enterNoOfUnits() throws IOException {
+		societyMaster_Class.EnterNoOfUnits();
+	}
+
+	@Test(priority = 10)
+	public void enterArea() throws IOException {
+		societyMaster_Class.EnterArea();
+	}
+
+	@Test(priority = 11)
+	public void enterDataVerifiedBy() throws IOException {
+		societyMaster_Class.EnterDataVerifiedBy();
+	}
+
+	@Test(priority = 12)
+	public void enterCTSNo() throws IOException {
+		societyMaster_Class.EnterCTSNo();
+	}
+
+	@Test(priority = 13)
+	public void clickOntheSaveButton() throws IOException {
+		societyMaster_Class.ClickOntheSaveButton();
+	}
+
 }
-  
